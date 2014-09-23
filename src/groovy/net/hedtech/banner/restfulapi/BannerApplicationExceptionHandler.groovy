087b76588f73da1e0a96c42cbe91bc527e765dec
Rasul C. Shishehbor <rasul.shishehbor@ellucian.com>
2014-09-22 20:36:43 -0400

    Updating BannerApplicationExceptionHandler

--- src/groovy/net/hedtech/banner/restfulapi/BannerApplicationExceptionHandler.groovy
+++ src/groovy/net/hedtech/banner/restfulapi/BannerApplicationExceptionHandler.groovy
@@ -4,8 +4,6 @@ import net.hedtech.restfulapi.ErrorResponse
 import net.hedtech.restfulapi.ExceptionHandlerContext
 import net.hedtech.restfulapi.exceptionhandlers.ApplicationExceptionHandler
 import net.hedtech.restfulapi.ExceptionHandler
-import org.springframework.validation.FieldError
-
 
 class BannerApplicationExceptionHandler extends ApplicationExceptionHandler implements ExceptionHandler {
 
@@ -13,25 +11,22 @@ class BannerApplicationExceptionHandler extends ApplicationExceptionHandler impl
     ErrorResponse handle(Throwable e, ExceptionHandlerContext context) {
         def response = super.handle(e, context)
         List<Map> newContent = []
-        //Handle BusinessLogicValidationException v2
-        if (response.content instanceof Map &&
-                response.content.hasProperty('messageCode') &&
-                response.content.hasProperty('message'))
-            newContent << ['code': response.content.messageCode,
+
+        if( response.content instanceof Map && response.content.errors ){
+            //Handle BusinessLogicValidationException v2
+            if( response.content.errors instanceof Map &&
+                    response.content.errors.hasProperty('messageCode') &&
+                    response.content.errors.hasProperty('message'))
+                newContent << ['code': response.content.messageCode,
                            'message': response.content.message.encodeAsHTML()]
-        //Handle various SQLExceptions or other exceptions where errors is populated.
-        else if( response.content && response.content.hasProperty('errors')) {
-              // Pre-localized API thrown exceptions
-            if( response.content.errors instanceof List<String> ) {
+            else if( response.content.errors instanceof List ) {
                 response.content.errors.each { it ->
-                    newContent << ['code': "Global.Internal.Error", 'message': it.encodeAsHTML()]
+                    if (it instanceof Map)
+                        newContent << ['code': "Global.Internal.Error", 'message': it.message?.encodeAsHTML()]
+                    if (it instanceof String)
+                        newContent << ['code': "Global.Internal.Error", 'message': it.encodeAsHTML()]
                 }
-            } //Multi-Model validation error
-            else if( response.content.errors instanceof List<FieldError> ) {
-                response.content.errors.each { it ->
-                    newContent << ['code': "Global.Internal.Error", 'message': it.message?.encodeAsHTML()]
-                }
-            } // SQL exception.getMessage()
+            }
             else if( response.content.errors instanceof String ) {
                 newContent << ['code': "Global.Internal.Error", 'message': response.content.errors.encodeAsHTML()]
             } //Some unexpected object found in errors.
@@ -39,7 +34,7 @@ class BannerApplicationExceptionHandler extends ApplicationExceptionHandler impl
                 newContent << ['code': "Global.Internal.Error", 'message': response.content.errors.toString().encodeAsHTML()]
             }
         } //Default to handle the rest.
-        else if( !response.content && response.hasProperty('message')) {
+        else if( response.hasProperty('message')) {
             newContent << ['code': "Global.Internal.Error", 'message': response.message.encodeAsHTML()]
         }
         if( newContent.size() > 0 )
