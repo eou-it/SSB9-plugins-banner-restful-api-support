4a73c6017a8ad4d5a91298c6467580ea99bdee56
Rasul C. Shishehbor <rasul.shishehbor@ellucian.com>
2014-10-20 23:39:49 -0400

    Enhancing support for schema defined error codes

--- src/groovy/net/hedtech/banner/restfulapi/BannerApplicationExceptionHandler.groovy
+++ src/groovy/net/hedtech/banner/restfulapi/BannerApplicationExceptionHandler.groovy
@@ -1,11 +1,15 @@
 package net.hedtech.banner.restfulapi
 
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import net.hedtech.restfulapi.ErrorResponse
 import net.hedtech.restfulapi.ExceptionHandlerContext
 import net.hedtech.restfulapi.exceptionhandlers.ApplicationExceptionHandler
 import net.hedtech.restfulapi.ExceptionHandler
 
 class BannerApplicationExceptionHandler extends ApplicationExceptionHandler implements ExceptionHandler {
+    static String SCHEMA_ERROR = "Global.SchemaValidation.Error"
+    static String SCHEMA_MESSAGE = "Errors parsing input JSON."
+    static String INTERNAL_ERROR = "Global.Internal.Error"
 
     @Override
     ErrorResponse handle(Throwable e, ExceptionHandlerContext context) {
@@ -13,29 +17,42 @@ class BannerApplicationExceptionHandler extends ApplicationExceptionHandler impl
         List<Map> newContent = []
 
         if( response.content instanceof Map && response.content.errors ){
-            //Handle BusinessLogicValidationException v2
-            if( response.content.errors instanceof Map &&
-                    response.content.errors.hasProperty('messageCode') &&
-                    response.content.errors.hasProperty('message'))
-                newContent << ['code': response.content.messageCode,
-                           'message': response.content.message.encodeAsHTML()]
-            else if( response.content.errors instanceof List ) {
-                response.content.errors.each { it ->
-                    if (it instanceof Map)
-                        newContent << ['code': "Global.Internal.Error", 'message': it.message?.encodeAsHTML()]
-                    if (it instanceof String)
-                        newContent << ['code': "Global.Internal.Error", 'message': it.encodeAsHTML()]
+            //Handle BusinessLogicValidationException
+            if( response.content.errors instanceof List ) {
+                response.content.errors.each { error ->
+                    if( error instanceof Map ) {
+                        if (error.containsKey('messageCode') &&
+                                error.containsKey('message')) {
+                            if (CH.config.restfulapi.apiErrorCodes.contains(error.messageCode)) {
+                                newContent << ['code'   : error.messageCode,
+                                               'message': error.message.encodeAsHTML(),
+                                               'description':error.message.encodeAsHTML()]
+                            } else {
+                                newContent << ['code'   : SCHEMA_ERROR,
+                                               'message': error.message.encodeAsHTML(),
+                                               'description': SCHEMA_MESSAGE]
+                            }
+                        }else {
+                            newContent << ['code': SCHEMA_ERROR, 'message': error.message?.encodeAsHTML(), 'description': SCHEMA_MESSAGE]
+                        }
+                    } else if (error instanceof String)
+                        newContent << ['code': SCHEMA_ERROR, 'message': error.encodeAsHTML(), 'description': SCHEMA_MESSAGE]
                 }
             }
             else if( response.content.errors instanceof String ) {
-                newContent << ['code': "Global.Internal.Error", 'message': response.content.errors.encodeAsHTML()]
+                newContent << ['code': SCHEMA_ERROR, 'message': response.content.errors.encodeAsHTML(), 'description': SCHEMA_MESSAGE]
             } //Some unexpected object found in errors.
             else {
-                newContent << ['code': "Global.Internal.Error", 'message': response.content.errors.toString().encodeAsHTML()]
+                newContent << ['code': SCHEMA_ERROR, 'message': response.content.errors.toString().encodeAsHTML(), 'description': SCHEMA_MESSAGE]
             }
         } //Default to handle the rest.
         else if( response.hasProperty('message')) {
-            newContent << ['code': "Global.Internal.Error", 'message': response.message.encodeAsHTML()]
+            if( response.httpStatusCode == 500 ) {
+                newContent << ['code': INTERNAL_ERROR, 'message': response.message.encodeAsHTML(), 'description': 'Unspecified Error on the system which prevented execution.']
+            }
+            else {
+                newContent << ['code': SCHEMA_ERROR, 'message': response.message.encodeAsHTML(), 'description': SCHEMA_MESSAGE]
+            }
         }
         if( newContent.size() > 0 )
             response.setContent([errors:newContent])
