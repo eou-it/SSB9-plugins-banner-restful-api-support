11fec2efb913e8b3a98e7f620bcef44d02ca2712
Brian Bell <brian.bell@ellucian.com>
2017-08-08 09:34:52 -0400

    Correct copyright errors

--- grails-app/services/net/hedtech/integration/resource/RepresentationResolutionService.groovy
+++ grails-app/services/net/hedtech/integration/resource/RepresentationResolutionService.groovy
@@ -1,3 +1,6 @@
+/******************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ ******************************************************************************/
 package net.hedtech.integration.resource
 
 import net.hedtech.integration.extension.ExtensionVersion
