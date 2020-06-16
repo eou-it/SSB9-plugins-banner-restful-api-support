ca3ed4879ad8bbc364ffea8856f4d10d2728a6d0
swaroopsg <swaroop.sg@ellucian.com>
2020-06-16 16:36:04 +0530

    modified facade service bean name for custom-resources

--- src/main/groovy/net/hedtech/banner/restfulapi/BannerRuntimeResourceDefinitions.groovy
+++ src/main/groovy/net/hedtech/banner/restfulapi/BannerRuntimeResourceDefinitions.groovy
@@ -43,7 +43,7 @@ class BannerRuntimeResourceDefinitions implements RestRuntimeResourceDefinitions
         ResourceConfig resourceConfig = new ResourceConfig();
 
         resourceConfig.name = resource
-        resourceConfig.serviceName = "genericCustomApiDataModelFacadeService"
+        resourceConfig.serviceName = "specDrivenApiDataModelFacadeService"
         resourceConfig.setMethods(['list', 'show'])
 
         JSONBeanMarshallerConfig jsonBeanMarshallerConfig = new JSONBeanMarshallerConfig();
